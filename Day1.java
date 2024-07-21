609517c35cf23d9fb6a279796311189fb9df01db
Lahar Gupta <lahar0916@gmail.com>
2024-07-21 08:57:18 +0530

    Add files via upload

---
+++ Day1.java
@@ -0,0 +1,14 @@
+package technicaltraining;
+
+public class Day1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//type casting
+		int a=1,b=4;
+		float result=(float )a/b;
+		System.out.println("Without type casting : "+b/a);
+		System.out.println("With type casting : " +result);
+	}
+
+}
